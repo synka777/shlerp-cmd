3af97d107bc11cbcfaea564128a697feb851b178
synchronic777 <m.barbegayet@gmail.com>
2024-05-11 18:02:53 +0200

    Fixed the logic & added support for symbolic links archiving

--- nodesave.py
+++ nodesave.py
@@ -6,9 +6,9 @@ Released under the MIT license
 import sys
 import os
 import shutil
-import pathlib
+import glob
 from datetime import datetime
-from zipfile import ZipFile, ZIP_DEFLATED
+from zipfile import ZipFile, ZIP_DEFLATED, ZipInfo
 
 
 def exists(path):
@@ -29,38 +29,40 @@ def get_files(path):
 
 def get_dt():
     """
-    This option will be useful if we want to use the cache option, to avoid downloading the npm packages again.
     :return: A timestamp in string format
     """
     return str(datetime.now().strftime('%d%m%y%H%M%S'))
 
 
-def build_archive(archive_name, src_path, dst_path):
-    """Archives all files from a project, excepted the node_modules folder
-    """
-    elem_list = get_files(src_path)
-    # TODO: See how to specify a correct output folder
-    with ZipFile(archive_name, 'w', ZIP_DEFLATED, compresslevel=9) as archive:
-        for elem in elem_list:
-            if not os.path.isdir(f'{src_path}{elem}'):
-                archive.write(f'{src_path}{elem}')
-            else:
-                directory = pathlib.Path(f'{src_path}{elem}')
-                for file_path in directory.rglob("*"):
-                    archive.write(file_path, arcname=file_path.relative_to(directory))
-    # TODO: Add a check to verify archive integrity
+def build_archive(project_fld, dst_path):
+    with ZipFile(f'{dst_path}.zip', 'w', ZIP_DEFLATED, compresslevel=9) as zip_archive:
+        for filename in glob.iglob(project_fld + '/**', recursive=True):
+            if 'node_modules' not in filename:
+                filename = filename.split(f'{project_fld}/')[1]
+                # If the filename is actually a symbolic link, use zip_info and zipfile.writestr()
+                # Source: https://gist.github.com/kgn/610907
+                if os.path.islink(filename):
+                    # http://www.mail-archive.com/python-list@python.org/msg34223.html
+                    zip_info = ZipInfo(filename)
+                    zip_info.create_system = 3
+                    # long type of hex val of '0xA1ED0000L',
+                    # say, symlink attr magic...
+                    zip_info.external_attr = 2716663808
+                    zip_archive.writestr(zip_info, os.readlink(f'./{filename}'))
+                else:
+                    zip_archive.write(f'./{filename}')
 
 
-def duplicate(path, dst, cache):
+def duplicate(elem_list, path, dst, cache):
     """Duplicates a project folder, processes all files and folders. node_modules will be processed last if cache = True
+    :param elem_list, list of strings that represents files and folder to process
     :param path, string that represents the project folder we want to duplicate
     :param dst, string that represents the destination folder where we will be copy the project files
     :param cache, boolean
     """
-    element_list = get_files(path)
     try:
         os.mkdir(dst)
-        for elem in element_list:
+        for elem in elem_list:
             orig = f'{path}/{elem}'
             full_dst = f'{dst}/{elem}'
             if os.path.isdir(orig):
@@ -88,33 +90,33 @@ def main(path, auto_inst, cache, archive):
     # 1. Check if the current folder is a javascript project
     if package_file:
         print('OK - package.json found')
-
+        elements = get_files(path)
         node_modules = exists(f'{path}/node_modules')
         dst = f'{path}_{get_dt()}'
 
         if archive:
             # If the -a switch is provided to the script, we use build_archive() and exclude the node_module folder
-            build_archive()
+            build_archive(path, dst)
         else:
             # Else if we don't want an archive we will do a copy of the project instead
             if not cache:
                 # Copy everything except the node_modules folder
-                duplicate(path, dst, False)
+                duplicate(elements, path, dst, False)
                 if auto_inst:
                     print('Installing npm packages...')
                     os.system('npm i')
             else:
                 if node_modules:
-                    duplicate(path, dst, True)
+                    duplicate(elements, path, dst, True)
                 else:
-                    duplicate(path, dst, False)
+                    duplicate(elements, path, dst, False)
     else:
         print(f'{path} is not a node project')
         exit()
 
 
 if __name__ == '__main__':
-    auto_install, use_cache, is_archive = False
+    auto_install = use_cache = is_archive = False
     folder = ''
     for index, arg in enumerate(sys.argv):
         if arg == '--cache' or arg == '-c':
